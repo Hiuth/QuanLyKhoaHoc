bb153838a25052ba8844aedde1510cc8632bd351
Hiuth <hiuth2004@gmail.com>
2024-11-10 10:56:05 +0700

    Initial commit

--- src/main/java/com/example/quanlykhoahoc/service/CourseService.java
+++ src/main/java/com/example/quanlykhoahoc/service/CourseService.java
@@ -1,9 +1,11 @@
 package com.example.quanlykhoahoc.service;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CourseService {
 
     @Autowired
